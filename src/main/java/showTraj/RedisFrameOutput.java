f28b45dd86da387910fca88f8a8b3cb6128c13a2
fuzhengjia <fuzhengjia@gmail.com>
2014-11-12 11:28:16 +0800

    show traj 9

--- src/main/java/showTraj/RedisFrameOutput.java
+++ src/main/java/showTraj/RedisFrameOutput.java
@@ -5,15 +5,20 @@ import backtype.storm.task.TopologyContext;
 import backtype.storm.topology.OutputFieldsDeclarer;
 import backtype.storm.topology.base.BaseRichBolt;
 import backtype.storm.tuple.Tuple;
+import org.bytedeco.javacpp.BytePointer;
 import topology.Serializable;
 import topology.StreamFrame;
 
 import java.util.Map;
 
+import static org.bytedeco.javacpp.opencv_core.CV_8UC1;
+import static org.bytedeco.javacpp.opencv_core.cvMat;
+import static org.bytedeco.javacpp.opencv_highgui.cvDecodeImage;
 import static topology.StormConfigManager.getInt;
 import static topology.StormConfigManager.getString;
 import static showTraj.Constant.*;
 
+import org.bytedeco.javacpp.opencv_core;
 /**
  * Created by Intern04 on 5/8/2014.
  */
@@ -53,7 +58,9 @@ public class RedisFrameOutput extends BaseRichBolt {
     public void execute(Tuple tuple) {
         int frameId = tuple.getIntegerByField(FIELD_FRAME_ID);
         Serializable.Mat sMat = (Serializable.Mat) tuple.getValueByField(FIELD_FRAME_MAT);
-        //opencv_core.Mat mat = sMat.toJavaCVMat();
+        byte[] imgBytes = sMat.toString().getBytes();
+        opencv_core.IplImage image = cvDecodeImage(cvMat(1, imgBytes.length, CV_8UC1, new BytePointer(imgBytes)));
+        opencv_core.Mat mat = sMat.toJavaCVMat();
         producer.addFrame(new StreamFrame(frameId, sMat.toJavaCVMat()));
 
         System.out.println("finishedAdd: " + System.currentTimeMillis() + ":" + frameId);
