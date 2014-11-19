fd923a584e557207a31ff6a8ca09d54b324142ad
fuzhengjia <fuzhengjia@gmail.com>
2014-11-19 17:30:28 +0800

    testVLD 1

---
+++ src/main/java/vldTest/testFrameSource.java
@@ -0,0 +1,68 @@
+package vldTest;
+
+import backtype.storm.spout.SpoutOutputCollector;
+import backtype.storm.task.TopologyContext;
+import backtype.storm.topology.OutputFieldsDeclarer;
+import backtype.storm.tuple.Fields;
+import backtype.storm.tuple.Values;
+import org.bytedeco.javacpp.BytePointer;
+import org.bytedeco.javacpp.opencv_core;
+import tool.RedisQueueSpout;
+import topology.Serializable;
+
+import java.util.Map;
+
+import static org.bytedeco.javacpp.opencv_core.CV_8UC1;
+import static org.bytedeco.javacpp.opencv_core.cvMat;
+import static org.bytedeco.javacpp.opencv_highgui.cvDecodeImage;
+import static tool.Constant.FIELD_FRAME_BYTES;
+import static tool.Constant.STREAM_FRAME_OUTPUT;
+import static topology.Constants.RAW_FRAME_STREAM;
+
+/**
+ * Created by ding on 14-7-3.
+ */
+public class testFrameSource extends RedisQueueSpout {
+
+    private int frameId;
+    //private String idPrefix;
+    opencv_core.IplImage image;
+    opencv_core.Mat mat;
+
+    public testFrameSource(String host, int port, String queue) {
+        super(host, port, queue, true);
+    }
+
+    @Override
+    public void open(Map conf, TopologyContext context, SpoutOutputCollector collector) {
+        super.open(conf, context, collector);
+        this.collector = collector;
+        frameId = 0;
+        //idPrefix = String.format("s-%02d-", context.getThisTaskIndex() + 1);
+    }
+
+
+    @Override
+    protected void emitData(Object data) {
+        //String id = idPrefix + frameId++;
+        String id = String.valueOf(frameId);
+
+        byte[] imgBytes = (byte[]) data;
+
+        opencv_core.IplImage image = cvDecodeImage(cvMat(1, imgBytes.length, CV_8UC1, new BytePointer(imgBytes)));
+
+        opencv_core.Mat mat = new opencv_core.Mat(image);
+        Serializable.Mat sMat = new Serializable.Mat(mat);
+
+        collector.emit(RAW_FRAME_STREAM, new Values(frameId, sMat, 0), id);
+
+        long nowTime = System.currentTimeMillis();
+        System.out.printf("Sendout: " + nowTime + "," + frameId);
+        frameId ++;
+    }
+
+    @Override
+    public void declareOutputFields(OutputFieldsDeclarer declarer) {
+        declarer.declareStream(RAW_FRAME_STREAM, new Fields("frameId", "frameMat", "patchCount"));
+    }
+}
---
+++ src/main/java/vldTest/testRedisFrameAggBolt.java
@@ -0,0 +1,116 @@
+package vldTest;
+
+import backtype.storm.task.OutputCollector;
+import backtype.storm.task.TopologyContext;
+import backtype.storm.topology.OutputFieldsDeclarer;
+import backtype.storm.topology.base.BaseRichBolt;
+import backtype.storm.tuple.Tuple;
+import logodetection.Debug;
+import logodetection.Util;
+import org.bytedeco.javacpp.opencv_core;
+import topology.RedisStreamProducer;
+import topology.Serializable;
+import topology.StreamFrame;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static topology.Constants.PROCESSED_FRAME_STREAM;
+import static topology.Constants.RAW_FRAME_STREAM;
+import static topology.StormConfigManager.getInt;
+import static topology.StormConfigManager.getString;
+
+/**
+ * Created by Intern04 on 5/8/2014.
+ */
+public class testRedisFrameAggBolt extends BaseRichBolt {
+    OutputCollector collector;
+
+    RedisStreamProducer producer;
+
+    //int lim = 31685; // SONY
+    private HashMap<Integer, List<Serializable.Rect>> processedFrames;
+    private HashMap<Integer, Serializable.Mat> frameMap;
+    private List<Serializable.Rect> listHistory;
+
+    private String host;
+    private int port;
+    private String queueName;
+    private int persistFrames;
+    private int accumulateFrameSize;
+
+    @Override
+    public void declareOutputFields(OutputFieldsDeclarer outputFieldsDeclarer) {
+
+    }
+
+    @Override
+    public void prepare(Map map, TopologyContext topologyContext, OutputCollector outputCollector) {
+
+        host = getString(map, "redis.host");
+        port = getInt(map, "redis.port");
+        queueName = getString(map, "redis.queueName");
+        processedFrames = new HashMap<>();
+        frameMap = new HashMap<>();
+        this.collector = outputCollector;
+
+        persistFrames = Math.max(getInt(map, "persistFrames"), 1);
+        accumulateFrameSize = Math.max(getInt(map, "accumulateFrameSize"), 1);
+        listHistory = null;
+
+        producer = new RedisStreamProducer(host, port, queueName, accumulateFrameSize);
+        new Thread(producer).start();
+
+    }
+
+    // Fields("frameId", "frameMat", "patchCount")
+    // Fields("frameId", "foundRectList")
+    @Override
+    public void execute(Tuple tuple) {
+        String streamId = tuple.getSourceStreamId();
+        int frameId = tuple.getIntegerByField("frameId");
+
+        if (streamId.equals(PROCESSED_FRAME_STREAM)) {
+            List<Serializable.Rect> list = (List<Serializable.Rect>) tuple.getValueByField("foundRectList");
+            if (!processedFrames.containsKey(frameId)) {
+                processedFrames.put(frameId, list);
+                //System.out.println("addtoProcessedFrames: " + System.currentTimeMillis() + ":" + frameId);
+            }
+        } else if (streamId.equals(RAW_FRAME_STREAM)) {
+            Serializable.Mat sMat = (Serializable.Mat) tuple.getValueByField("frameMat");
+            if (!frameMap.containsKey(frameId)) {
+                frameMap.put(frameId, sMat);
+                //System.out.println("addtoFrameMap: " + System.currentTimeMillis() + ":" + frameId);
+            } else {
+                if (Debug.topologyDebugOutput)
+                    System.err.println("FrameAggregator: Received duplicate message");
+            }
+        }
+
+        if (frameMap.containsKey(frameId) && processedFrames.containsKey(frameId)) {
+            opencv_core.Mat mat = frameMap.get(frameId).toJavaCVMat();
+            List<Serializable.Rect> list = processedFrames.get(frameId);
+
+            if (frameId % persistFrames == 0) {
+                listHistory = list;
+            } else if (listHistory == null) {
+                listHistory = list;
+            } else {
+                list = listHistory;
+            }
+
+            if (list != null) {
+                for (Serializable.Rect rect : list) {
+                    Util.drawRectOnMat(rect.toJavaCVRect(), mat, opencv_core.CvScalar.MAGENTA);
+                }
+            }
+            producer.addFrame(new StreamFrame(frameId, mat));
+            System.out.println("finishedAdd: " + System.currentTimeMillis() + ":" + frameId);
+            processedFrames.remove(frameId);
+            frameMap.remove(frameId);
+        }
+        System.out.println("finished: " + System.currentTimeMillis() + ":" + frameId);
+        collector.ack(tuple);
+    }
+}
