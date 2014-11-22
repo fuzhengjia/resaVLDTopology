1d354eb6e1305983ba8a31956f8d3a6d0e814855
fuzhengjia <fuzhengjia@gmail.com>
2014-11-22 18:57:40 +0800

    Nov-22, update TomVideoStreamReceiverForLinux, add fps to control frame retrieve rate dump from Redis.

--- src/main/java/server/TomVideoStreamReceiverForLinux.java
+++ src/main/java/server/TomVideoStreamReceiverForLinux.java
@@ -54,17 +54,62 @@ public class TomVideoStreamReceiverForLinux {
         }
     }
 
+    public void VideoStreamReceiver(int fps) throws IOException, FrameGrabber.Exception, InterruptedException {
+
+        // Caution for the command line usage!!! pipeline
+        //
+        // storm jar target/resa-vld-1.0-SNAPSHOT-jar-with-dependencies.jar server.TomVideoStreamReceiverForLinux 192.168.0.30 6379 tomQ
+        // | ffmpeg -f image2pipe -codec mjpeg -i pipe:0 -r 25 http://192.168.0.30:8090/feed2.ffm
+
+        Jedis jedis = getConnectedJedis();
+        byte[] baData = null;
+        //OutputStream ffmpegInput = p.getOutputStream();
+        int generatedFrames = 0;
+        long start = System.currentTimeMillis();
+        long last = start;
+        while (true) {
+            try {
+
+                baData = jedis.lpop(queueName);
+
+                if (baData != null) {
+                    BufferedImage bufferedImageRead = ImageIO.read(new ByteArrayInputStream(baData));
+                    //ImageIO.write(bufferedImageRead, "JPEG", ffmpegInput);
+                    ImageIO.write(bufferedImageRead, "JPEG", System.out);
+                    generatedFrames++;
+                    if (generatedFrames % fps == 0) {
+                        long current = System.currentTimeMillis();
+                        long elapse = current - last;
+                        long remain = 1000 - elapse;
+                        if (remain > 0) {
+                            Thread.sleep(remain);
+                        }
+                        last = System.currentTimeMillis();
+                    }
+                }
+            } catch (Exception e) {
+                System.out.println(e.getStackTrace());
+                disconnect();
+            }
+        }
+    }
+
     // | ffmpeg -f image2pipe -codec mjpeg -i pipe:0 -r 25 http://192.168.0.30:8090/feed2.ffm
     public static void main(String args[]) {
 
         if (args.length < 3) {
-            System.out.println("usage: TomVideoStreamReceiverForLinux <Redis host> <Redis port> <Redis Queue>");
+            System.out.println("usage: TomVideoStreamReceiverForLinux <Redis host> <Redis port> <Redis Queue> [fps]");
             return;
         }
         //TomVideoStreamReceiverForLinux tvsr = new TomVideoStreamReceiverForLinux("192.168.0.30", 6379, "tomQ");
         TomVideoStreamReceiverForLinux tvsr = new TomVideoStreamReceiverForLinux(args[0], Integer.parseInt(args[1]), args[2]);
         try {
-            tvsr.VideoStreamReceiver();
+            if (args.length == 4){
+                int fps = Integer.parseInt(args[3]);
+                tvsr.VideoStreamReceiver(fps);
+            } else {
+                tvsr.VideoStreamReceiver();
+            }
         } catch (Exception e) {
         }
     }
