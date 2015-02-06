311e1fbc202e33e272736b21e13f49334a2af451
fuzhengjia <fuzhengjia@gmail.com>
2015-02-06 23:28:58 +0800

    Test traj display topology 16

--- src/main/java/generateTraj/frameDisplay.java
+++ src/main/java/generateTraj/frameDisplay.java
@@ -33,8 +33,7 @@ public class frameDisplay extends BaseRichBolt {
     OutputCollector collector;
     RedisStreamProducer producer;
 
-    //private HashMap<Integer, Serializable.Mat> rawFrameMap;
-    private HashMap<Integer, opencv_core.IplImage> rawFrameMap;
+    private HashMap<Integer, Serializable.Mat> rawFrameMap;
     private HashMap<Integer, List<TraceRecord>> traceData;
 
     private String host;
@@ -78,12 +77,12 @@ public class frameDisplay extends BaseRichBolt {
 
         if (streamId.equals(STREAM_FRAME_OUTPUT)){
 
-            IplImage fake = new IplImage();
+            //IplImage fake = new IplImage();
             Serializable.Mat sMat = (Serializable.Mat) tuple.getValueByField(FIELD_FRAME_MAT);
-            IplImage frame = sMat.toJavaCVMat().asIplImage();
+            //IplImage frame = sMat.toJavaCVMat().asIplImage();
             //rawFrameMap.computeIfAbsent(frameId, k->frame);
             if (!rawFrameMap.containsKey(frameId)){
-                rawFrameMap.put(frameId, frame);
+                rawFrameMap.put(frameId, sMat);
             }
         } else if (streamId.equals(STREAM_PLOT_TRACE)){
             List<TraceRecord> traceRecords = (List<TraceRecord>)tuple.getValueByField(FIELD_TRACE_RECORD);
@@ -91,9 +90,9 @@ public class frameDisplay extends BaseRichBolt {
         }
 
         if (rawFrameMap.containsKey(frameId) && traceData.containsKey(frameId)){
-            ///opencv_core.Mat orgMat = rawFrameMap.get(frameId).toJavaCVMat();
-            IplImage frame = rawFrameMap.get(frameId);
-            ///IplImage frame = orgMat.asIplImage();
+            opencv_core.Mat orgMat = rawFrameMap.get(frameId).toJavaCVMat();
+            ///IplImage frame = rawFrameMap.get(frameId);
+            IplImage frame = orgMat.asIplImage();
             List<TraceRecord> traceRecords = traceData.get(frameId);
             for (int i = 0; i < traceRecords.size(); i++) {
                 TraceRecord trace = traceRecords.get(i);
