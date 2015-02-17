7e4453ef5239d4ad396270a76c65d2d9cb3a57f1
fuzhengjia <fuzhengjia@gmail.com>
2015-02-17 12:05:50 +0800

    Test traj display topology Gamma (traceCnt), V3b

--- src/main/java/generateTraj/tomTrajDisplayTopGamma2.java
+++ src/main/java/generateTraj/tomTrajDisplayTopGamma2.java
@@ -8,8 +8,6 @@ import backtype.storm.generated.StormTopology;
 import backtype.storm.topology.TopologyBuilder;
 import backtype.storm.tuple.Fields;
 import showTraj.RedisFrameOutput;
-import showTraj.RedisFrameOutputRC;
-import tool.FrameImplImageSource;
 import tool.FrameImplImageSourceGamma;
 import topology.Serializable;
 
@@ -87,9 +85,7 @@ public class tomTrajDisplayTopGamma2 {
                 .setNumTasks(getInt(conf, frameDisplay + ".tasks"));
 
         builder.setBolt(redisFrameOut, new RedisFrameOutput(), getInt(conf, redisFrameOut + ".parallelism"))
-                //.globalGrouping(frameDisplay, STREAM_FRAME_DISPLAY)
-                //.globalGrouping(spoutName, STREAM_FRAME_OUTPUT)
-                .globalGrouping(redisFrameOut, STREAM_FRAME_OUTPUT)
+                .globalGrouping(frameDisplay, STREAM_FRAME_DISPLAY)
                 .setNumTasks(getInt(conf, redisFrameOut + ".tasks"));
 
         StormTopology topology = builder.createTopology();
