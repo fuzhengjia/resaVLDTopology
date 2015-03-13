e5a8f9e7c770268b6e1a7d751e059a2fcda785e4
fuzhengjia <fuzhengjia@gmail.com>
2015-03-13 11:39:49 +0800

    tVLD-beta, test 7

--- src/main/java/topology/tFrameSourceBeta.java
+++ src/main/java/topology/tFrameSourceBeta.java
@@ -5,10 +5,15 @@ import backtype.storm.task.TopologyContext;
 import backtype.storm.topology.OutputFieldsDeclarer;
 import backtype.storm.tuple.Fields;
 import backtype.storm.tuple.Values;
+import org.bytedeco.javacpp.BytePointer;
+import org.bytedeco.javacpp.opencv_core;
+import org.bytedeco.javacpp.opencv_highgui;
+import org.bytedeco.javacpp.opencv_imgproc;
 import tool.RedisQueueSpout;
 
 import java.util.Map;
 
+import static org.bytedeco.javacpp.opencv_core.cvMat;
 import static topology.Constants.*;
 
 /**
@@ -35,7 +40,13 @@ public class tFrameSourceBeta extends RedisQueueSpout {
     protected void emitData(Object data) {
         //String id = idPrefix + frameId++;
         String id = String.valueOf(frameId);
-        collector.emit(RAW_FRAME_STREAM, new Values(frameId, data), id);
+        byte[] imgBytes = (byte[]) data;
+        opencv_core.IplImage image = opencv_highgui.cvDecodeImage(opencv_core.cvMat(1, imgBytes.length, opencv_core.CV_8UC1, new BytePointer(imgBytes)));
+
+        opencv_core.Mat mat = new opencv_core.Mat(image);
+        Serializable.Mat sMat = new Serializable.Mat(mat);
+
+        collector.emit(RAW_FRAME_STREAM, new Values(frameId, sMat), id);
 
         long nowTime = System.currentTimeMillis();
         System.out.printf("Sendout: " + nowTime + "," + frameId);
