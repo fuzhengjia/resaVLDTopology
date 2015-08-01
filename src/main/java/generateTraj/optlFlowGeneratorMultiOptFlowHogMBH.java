a198b73581273ec298dac4e3050ed69457421908
fuzhengjia <fuzhengjia@gmail.com>
2015-08-01 16:36:37 +0800

    actDet, seeking for bugs, 7

--- src/main/java/generateTraj/optlFlowGeneratorMultiOptFlowHogMBH.java
+++ src/main/java/generateTraj/optlFlowGeneratorMultiOptFlowHogMBH.java
@@ -26,7 +26,7 @@ import static tool.Constants.*;
  */
 public class optlFlowGeneratorMultiOptFlowHogMBH extends BaseRichBolt {
     OutputCollector collector;
-    IplImage grey, prev_grey;
+    //IplImage grey, prev_grey;
 
 //    float scale_stride;
 //    int scale_num;
@@ -43,8 +43,8 @@ public class optlFlowGeneratorMultiOptFlowHogMBH extends BaseRichBolt {
     public void prepare(Map map, TopologyContext topologyContext, OutputCollector outputCollector) {
         this.collector = outputCollector;
 
-        this.grey = null;
-        this.prev_grey = null;
+        //this.grey = null;
+        //this.prev_grey = null;
         hogInfo = new DescInfo(8, 0, 1, patch_size, nxy_cell, nt_cell, min_flow);
         mbhInfo = new DescInfo(8, 0, 1, patch_size, nxy_cell, nt_cell, min_flow);
     }
@@ -55,14 +55,18 @@ public class optlFlowGeneratorMultiOptFlowHogMBH extends BaseRichBolt {
         int frameId = tuple.getIntegerByField(FIELD_FRAME_ID);
         IplImage imageFK = new IplImage();
         Serializable.Mat sMat = (Serializable.Mat) tuple.getValueByField(FIELD_FRAME_MAT);
-        this.grey = sMat.toJavaCVMat().asIplImage();
+        //this.grey = sMat.toJavaCVMat().asIplImage();
+        //IplImage grey_temp = cvCloneImage(this.grey);
+        IplImage grey_temp = cvCloneImage(sMat.toJavaCVMat().asIplImage());
 
         Serializable.Mat sMatPrev = (Serializable.Mat) tuple.getValueByField(FIELD_FRAME_MAT_PREV);
-        this.prev_grey = sMatPrev.toJavaCVMat().asIplImage();
+        //this.prev_grey = sMatPrev.toJavaCVMat().asIplImage();
+        //IplImage prev_grey_temp = cvCloneImage(this.prev_grey);
+        IplImage prev_grey_temp = cvCloneImage(sMatPrev.toJavaCVMat().asIplImage());
 
-        IplImage flow = cvCreateImage(cvGetSize(this.grey), IPL_DEPTH_32F, 2);
+        IplImage flow = cvCreateImage(cvGetSize(grey_temp), IPL_DEPTH_32F, 2);
 
-        opencv_video.cvCalcOpticalFlowFarneback(this.prev_grey, this.grey, flow,
+        opencv_video.cvCalcOpticalFlowFarneback(prev_grey_temp, grey_temp, flow,
                 Math.sqrt(2.0) / 2.0, 5, 10, 2, 7, 1.5, opencv_video.OPTFLOW_FARNEBACK_GAUSSIAN);
 
         Mat fMat = new Mat(flow);
@@ -70,15 +74,19 @@ public class optlFlowGeneratorMultiOptFlowHogMBH extends BaseRichBolt {
 
         collector.emit(STREAM_OPT_FLOW, tuple, new Values(frameId, sfMat));
 
-        int width = this.grey.width();
-        int height = this.grey.height();
+        int width = prev_grey_temp.width();
+        int height = prev_grey_temp.height();
         DescMat[] mbhMatXY = MbhComp(flow, mbhInfo, width, height);
         DescMat mbhMatX = mbhMatXY[0];
         DescMat mbhMatY = mbhMatXY[1];
-        DescMat hogMat = HogComp(this.prev_grey, hogInfo, width, height);
+        DescMat hogMat = HogComp(prev_grey_temp, hogInfo, width, height);
 
         collector.emit(STREAM_FEATURE_FLOW, tuple, new Values(frameId, new DescMat[] {hogMat, mbhMatX, mbhMatY}));
         collector.ack(tuple);
+
+        cvRelease(grey_temp);
+        cvRelease(prev_grey_temp);
+        cvRelease(flow);
     }
 
     @Override
